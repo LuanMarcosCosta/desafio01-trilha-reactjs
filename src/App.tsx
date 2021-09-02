8e7324c000bee4b96b18338ec6d3219827c55976
LuanMarcosCosta <luanmarcoscosta@outlook.com>
2021-09-02 06:55:48 -0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
-
-
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
-
-import { api } from './services/api';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import './styles/global.scss';
 
-import './styles/sidebar.scss';
-import './styles/content.scss';
 
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
+export function App() {
+ return 
+ <>
+ <SideBar/>
+ <Content />
+ </>
 }
-
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
